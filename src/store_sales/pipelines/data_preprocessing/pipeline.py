8caf4630c770a41d772af7df092040e1a50bc219
BWallyn <ben96.wallyn@gmail.com>
2025-05-02 23:09:42 +0200

    feat: Create node to merge transaction to train set

--- src/store_sales/pipelines/data_preprocessing/pipeline.py
+++ src/store_sales/pipelines/data_preprocessing/pipeline.py
@@ -4,7 +4,20 @@ generated using Kedro 0.19.12
 """
 
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from store_sales.pipelines.data_preprocessing.nodes import (
+    merge_transactions
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        pipe=[
+            node(
+                func=merge_transactions,
+                inputs=["df_train", "df_transactions"],
+                outputs="df_train_w_transactions",
+                name="Merge_transactions_to_train",
+            ),
+        ],
+        inputs=["df_train", "df_transactions"],
+    )
